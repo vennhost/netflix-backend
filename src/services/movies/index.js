ea53966d0df5e9f712e50672349c8f8ca3f12256
vennhost <samogconsult@gmail.com>
2019-12-26 13:14:26 +0100

    Basic CRUD completed

--- src/services/movies/index.js
+++ src/services/movies/index.js
@@ -23,7 +23,13 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     const movies = await getMovies()
     const movie = movies.find(movie => movie.imdbID === req.params.id)
-    res.send(movie)
+    if (movie) {
+        res.send(movie)
+    }
+    else {
+        res.status(404).send(`Movie ${req.params.id} not found`)
+    }
+    
 })
 
 router.post("/", async (req, res) => {
@@ -37,6 +43,38 @@ router.post("/", async (req, res) => {
     await fs.writeFile(filePath, JSON.stringify(movies))
 
     res.send("created")
+})
+
+router.put("/:id", async (req, res) => {
+    const movies = await getMovies()
+    const movie = movies.find(movie => movie.imdbID === req.params.id)
+
+    if (movie) {
+        const position = movies.indexOf(movie);
+        const movieUpdated = Object.assign(movie, req.body)
+        movies[position] = movieUpdated;
+        await fs.writeFile(filePath, JSON.stringify(movies))
+        res.send(`movie ${movie.imdbID} update successful`)
+    } else {
+        res.send("Movie not found")
+    }
+    
+})
+
+router.delete("/:id", async (req, res) => {
+    const movies = await getMovies()
+    const moviesRemaining = movies.filter(movie => movie.imdbID !== req.params.id)
+
+   
+
+    if (moviesRemaining.length === movies.length) {
+        res.status(404).send(`Movie ${req.params.id} can not be found`)
+    }
+     else {
+         await fs.writeFile(filePath, JSON.stringify(moviesRemaining))
+        res.send("Deleted Succesfully")
+    }
+    
 })
 
 
